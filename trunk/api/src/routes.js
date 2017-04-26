2317d77f5a43294ab1b37ed4fadee29191e03073
giricgoyal <giricgoyal@gmail.com>
2017-04-26 11:41:31 +0530

    api init files

---
+++ trunk/api/src/routes.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var rootRoutes = function(app) {
+    var contentType = require('./middleware/errorHandler');
+    app.use(contentType.handleContentType);
+
+    // users route
+    app.use('/api/users', require('./users/route'));
+    app.use('/api/oauth', require('./auth/route'));
+
+    // movie routes
+    // app.use('/api/movies', require('./movies/route'));
+};
+
+module.exports = rootRoutes;
